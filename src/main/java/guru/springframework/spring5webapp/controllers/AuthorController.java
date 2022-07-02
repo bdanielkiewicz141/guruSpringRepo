bce0ee21e87f96755af79ada0382456baec9acef
bdani <b.danielkiewicz141@gmail.com>
2022-07-02 17:55:12 +0200

    adding controllers
    warning - 500 error with AuthorController

--- src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -18,6 +18,6 @@ public class AuthorController {
 
         model.addAttribute("authors", authorRepository.findAll());
 
-        return "templates/authors/list1";
+        return "authors/List";
     }
 }
